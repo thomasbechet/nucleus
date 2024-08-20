eaf2abffec78ce53e4573dd1fde1dd50fe4f3db9
Thomas BECHET <thomas.bechet.pro@gmail.com>
2024-08-20 08:19:46 +0200

    ui api design

--- nucleus/utils/ui.h
+++ nucleus/utils/ui.h
@@ -1,16 +1,76 @@
 #ifndef NU_UI_H
 #define NU_UI_H
 
+#include <nucleus/graphics.h>
+
 typedef enum
 {
-    NU_UI_BUTTON,
-    NU_UI_CHECKBOX,
-    NU_UI
-} nu_ui_type_t;
+    NU_UI_CONTROLLER_DISABLED,
+    NU_UI_CONTROLLER_SELECTION,
+    NU_UI_CONTROLLER_CURSOR,
+} nu_ui_controller_mode_t;
 
 typedef struct
 {
+    nu_u32_t top;
+    nu_u32_t bottom;
+    nu_u32_t left;
+    nu_u32_t right;
+} nu__ui_margin_t;
 
-} nu_ui_element_t;
+typedef struct
+{
+    nu_material_handle_t material;
+    nu_texture_handle_t  texture;
+    nu_rect_t            extent;
+    nu__ui_margin_t      margin;
+    nu_bool_t            center;
+} nu__ui_image_t;
+
+typedef struct
+{
+    nu_input_handle_t       click;
+    nu_input_handle_t       move_up;
+    nu_input_handle_t       move_down;
+    nu_input_handle_t       move_left;
+    nu_input_handle_t       move_right;
+    nu_ui_controller_mode_t mode;
+    nu_ivec2_t              cursor;
+} nu__ui_controller_t;
+
+typedef union
+{
+    nu_renderpass_handle_t renderpass;
+    nu__ui_controller_t    controller;
+    nu__ui_image_t         image;
+} nu__ui_object_t;
+
+typedef nu_slotmap(nu__ui_object_t) nu__ui_object_slotmap_t;
+
+typedef struct
+{
+    nu__ui_object_slotmap_t _objects;
+    nu_slot_t               _main_renderpass;
+    nu_slot_t               _first_controller;
+
+    nu_bool_t      _building;
+    nu_renderer_t *_renderer; // Not null in build phase
+    nu_slot_t      _active_renderpass;
+} nu_ui_t;
+
+NU_API nu_error_t nu_ui_init(nu_allocator_t *alloc, nu_ui_t *ui);
+NU_API void       nu_ui_free(nu_ui_t *ui, nu_allocator_t *alloc);
+
+NU_API void nu_ui_begin(nu_ui_t       *ui,
+                        nu_platform_t *platform,
+                        nu_renderer_t *renderer);
+NU_API void nu_ui_end(nu_ui_t *ui);
+NU_API void nu_ui_submit_renderpasses(const nu_ui_t                *ui,
+                                      nu_renderer_t                *renderer,
+                                      const nu_renderpass_submit_t *info);
+
+NU_API nu_renderpass_handle_t nu_ui_active_renderpass(const nu_ui_t *ui);
+
+NU_API nu_bool_t nu_ui_button(nu_ui_t *ui, nu_rect_t r);
 
 #endif
--- nucleus/utils/ui_impl.h
+++ nucleus/utils/ui_impl.h
@@ -3,4 +3,70 @@
 
 #include <nucleus/utils/ui.h>
 
+nu_error_t
+nu_ui_init (nu_allocator_t *alloc, nu_ui_t *ui)
+{
+    nu_slotmap_init(&ui->_objects, alloc, 32);
+    ui->_first_controller  = NU_NULL;
+    ui->_active_renderpass = NU_NULL;
+    ui->_building          = NU_FALSE;
+    return NU_ERROR_NONE;
+}
+void
+nu_ui_free (nu_ui_t *ui, nu_allocator_t *alloc)
+{
+    nu_slotmap_free(&ui->_objects, alloc);
+}
+
+static nu_renderpass_handle_t
+nu__ui_active_renderpass (nu_ui_t *ui, nu_slot_t slot)
+{
+    if (slot)
+    {
+        return nu_slotmap_get(&ui->_objects, slot)->renderpass;
+    }
+    nu_renderpass_info_t info;
+    info.type               = NU_RENDERPASS_CANVAS;
+    info.reset_after_submit = NU_FALSE;
+    nu_renderpass_handle_t handle;
+    nu_error_t error = nu_renderpass_create(ui->_renderer, &info, &handle);
+    NU_ERROR_ASSERT(error);
+
+    return handle;
+}
+
+void
+nu_ui_begin (nu_ui_t *ui, nu_platform_t *platform, nu_renderer_t *renderer)
+{
+    ui->_building          = NU_TRUE;
+    ui->_renderer          = renderer;
+    ui->_active_renderpass = ui->_active_renderpass;
+
+    // Reset passes
+}
+void
+nu_ui_end (nu_ui_t *ui)
+{
+    NU_ASSERT(ui->_building);
+}
+void
+nu_ui_submit_renderpasses (const nu_ui_t                *ui,
+                           nu_renderer_t                *renderer,
+                           const nu_renderpass_submit_t *info)
+{
+}
+
+nu_renderpass_handle_t
+nu_ui_active_renderpass (const nu_ui_t *ui)
+{
+    NU_ASSERT(ui->_building);
+    return nu_slotmap_get(&ui->_objects, ui->_active_renderpass)->renderpass;
+}
+
+nu_bool_t
+nu_ui_button (nu_ui_t *ui, nu_rect_t r)
+{
+    return NU_FALSE;
+}
+
 #endif
