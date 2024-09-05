64a18f7a8f55284eacf021a6deca0bf7bec97109
Thomas BECHET <thomas.bechet.pro@gmail.com>
2024-09-05 07:31:49 +0200

    add header only generation script

---
+++ tools/generate_header_only.py
@@ -0,0 +1,25 @@
+import os
+import re
+
+included_files = set()
+line_count = 0
+output = open('nucleus.h', 'w')
+
+def process_file(path):
+    global line_count
+    if path in included_files:
+        return
+    included_files.add(path)
+    with open(path) as file:
+        for line in file.readlines():
+            m = re.match("\\#include\\s*\\<(.*)\\>", line)
+            if m:
+                child_file = m.group(1)
+                if child_file.startswith('nucleus'):
+                    process_file(child_file)
+            elif line.strip():
+                output.write(line)
+                line_count += 1
+
+process_file('nucleus/nucleus.h')
+print('line count:', line_count)
